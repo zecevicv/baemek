18caa822fdc4a7a1835d9c32fa5cae20e8a84351
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-06-29 14:02:03 +0200

    [ADD] - Homepage

--- js/app.js
+++ js/app.js
@@ -27,3 +27,92 @@ if (hamburgerBtn) {
     body.classList.toggle('no-scroll');
   });
 }
+
+// Close on outside click
+document.addEventListener('click', (e) => {
+  if (document.querySelector('.header.show-menu') && !e.target.closest('.header')) {
+    header.classList.remove('show-menu');
+    body.classList.remove('no-scroll');
+  }
+});
+
+/* #Tabs
+  ======================================================= */
+const tabs = document.querySelectorAll('.tabs');
+
+if (tabs) {
+  tabs.forEach((tab) => {
+    const tabBtns = tab.querySelectorAll('.tab-btn');
+    const tabPanels = tab.querySelectorAll('.tab-panel');
+
+    tabBtns.forEach((tabBtn) => {
+      tabBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const tabPanel = tab.querySelector(tabBtn.dataset.tab);
+
+        tabBtns.forEach((btn) => {
+          if (btn == tabBtn) {
+            btn.classList.add('active');
+          } else {
+            btn.classList.remove('active');
+          }
+        });
+
+        tabPanels.forEach((panel) => {
+          if (panel == tabPanel) {
+            panel.classList.add('active');
+          } else {
+            panel.classList.remove('active');
+          }
+        });
+
+      });
+    });
+  });
+}
+
+/* #Sliders
+  ======================================================= */
+
+window.addEventListener('load', () => {
+  // Posts Slider
+  if (document.querySelector('.posts-slider .swiper-container')) {
+    new Swiper('.posts-slider .swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.posts-slider .swiper-pagination',
+      },
+      breakpoints: {
+        0: {
+          allowTouchMove: true,
+          slidesPerView: 1.25
+        },
+        1023: {
+          allowTouchMove: false,
+          slidesPerView: 3
+        }
+      }
+    });
+  }
+
+  // Posts Slider
+  if (document.querySelector('.clients .swiper-container')) {
+    new Swiper('.clients .swiper-container', {
+      breakpoints: {
+        0: {
+          allowTouchMove: true,
+          slidesPerView: 2,
+          centeredSlides: true,
+          loop: true
+        },
+        1023: {
+          allowTouchMove: false,
+          slidesPerView: 11,
+          centeredSlides: false,
+          loop: false
+        }
+      }
+    });
+  }
+});
