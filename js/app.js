05a7d6471f68af64b695287fa2e8e304aba52cb8
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-06-30 16:31:34 +0200

    [ADD] - Product Page

--- js/app.js
+++ js/app.js
@@ -72,6 +72,52 @@ if (tabs) {
   });
 }
 
+/* #Accordion
+================================================== */
+const accordions = document.querySelectorAll('.accordion');
+
+if (accordions) {
+  accordions.forEach((accordion) => {
+    const collapsibles = accordion.querySelectorAll('.collapsible');
+
+    accordion.addEventListener('click', (e) => {
+      if (e.target.closest('.collapse-toggler')) {
+        e.preventDefault();
+
+        const collapsible = e.target.closest('.collapsible');
+        const collapse = collapsible.querySelector('.collapse');
+
+        if (!accordion.classList.contains('no-close')) {
+          collapsibles.forEach((coll) => {
+            if (coll.classList.contains('show') && coll != collapsible) {
+              collCollapse = coll.querySelector('.collapse');
+
+              coll.classList.remove('show');
+              gsap.to(collCollapse, {
+                height: '0'
+              });
+            }
+          });
+        }
+
+        collapsible.classList.toggle('show');
+
+        if (collapsible.classList.contains('show')) {
+          gsap.to(collapse, {
+            height: 'auto',
+            duration: .4,
+            ease: 'power1.inOut'
+          });
+        } else {
+          gsap.to(collapse, {
+            height: '0'
+          });
+        }
+      }
+    });
+  })
+}
+
 /* #Sliders
   ======================================================= */
 
@@ -109,6 +155,47 @@ window.addEventListener('load', () => {
         1023: {
           allowTouchMove: false,
           slidesPerView: 11,
+          centeredSlides: false,
+          loop: false
+        }
+      }
+    });
+  }
+
+  // Products Slider
+  if (document.querySelector('.products-slider .swiper-container')) {
+    new Swiper('.products-slider .swiper-container', {
+      navigation: {
+        nextEl: '.products-slider .swiper-next',
+        prevEl: '.products-slider .swiper-prev',
+      },
+      loop: true,
+      breakpoints: {
+        0: {
+          slidesPerView: 1.5,
+          centeredSlides: true
+        },
+        1023: {
+          slidesPerView: 3,
+          centeredSlides: false
+        }
+      }
+    });
+  }
+
+  // Posts Slider
+  if (document.querySelector('.product-info .swiper-container')) {
+    new Swiper('.product-info .swiper-container', {
+      breakpoints: {
+        0: {
+          allowTouchMove: true,
+          slidesPerView: 1.35,
+          centeredSlides: true,
+          loop: true
+        },
+        1023: {
+          allowTouchMove: false,
+          slidesPerView: 4,
           centeredSlides: false,
           loop: false
         }
